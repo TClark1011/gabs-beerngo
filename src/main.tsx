6aadcb2b588c74acdf33999be32d53c4876ca0e1
Thomas Clark <thomassiclark@gmail.com>
2025-05-31 14:02:58 +1000

    error handling

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import { theme } from "@/theme";
 import { DevTools } from "@/dev-tools";
 import { useAtomValue } from "jotai";
 import { beerReviewsAtom } from "@/stores.ts";
-import { ErrorBoundary } from "react-error-boundary";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "@/generated/route-tree.gen";
 
@@ -31,35 +30,40 @@ declare module "@tanstack/react-router" {
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<ErrorBoundary
-			fallback={
-				<div>
-					<p>
-						Uh oh!, there was an error. Please refresh the page and try again.
-						If the problem persists, try pressing the button below to reset your
-						data.
-					</p>
-					<button
-						onClick={() => {
-							if (confirm("Are you sure you want to reset your data?")) {
-								localStorage.clear();
-								location.reload();
-							}
-						}}
-					>
-						Reset Data
-					</button>
-				</div>
-			}
-		>
-			<ChakraProvider theme={theme}>
-				<Suspense>
-					<DevTools />
-				</Suspense>
-				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
-				<SideEffects />
-				<RouterProvider router={router} />
-			</ChakraProvider>
-		</ErrorBoundary>
+		<ChakraProvider theme={theme}>
+			<Suspense>
+				<DevTools />
+			</Suspense>
+			<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			<SideEffects />
+			<RouterProvider
+				router={router}
+				defaultErrorComponent={() => (
+					<div>
+						<p>
+							Uh oh!, there was an error. Please refresh the page and try again.
+							If the problem persists, try pressing the button below to reset
+							your data.
+						</p>
+						<button
+							style={{
+								backgroundColor: "red",
+								color: "white",
+								padding: 8,
+								borderRadius: 4,
+							}}
+							onClick={() => {
+								if (confirm("Are you sure you want to reset your data?")) {
+									localStorage.clear();
+									location.reload();
+								}
+							}}
+						>
+							Reset Data
+						</button>
+					</div>
+				)}
+			/>
+		</ChakraProvider>
 	</React.StrictMode>,
 );
